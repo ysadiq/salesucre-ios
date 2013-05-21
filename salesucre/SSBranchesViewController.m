7e9da626c28aafa51d7e84e71bcfb93b332eae4c
Haitham Reda <hreda@olitintl.com>
2013-05-22 00:30:10 +0200

    - instead of reloading table, using eye-candy tableview animation to delete/add/update

--- salesucre/SSBranchesViewController.m
+++ salesucre/SSBranchesViewController.m
@@ -120,6 +120,11 @@
     return cell;
 }
 
+- (void)configureCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath {
+    Branch *updatedBranch = [_fetchedResultsController objectAtIndexPath:indexPath];
+    [(SSCell *)cell setBranchDetails:updatedBranch];
+}
+
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
     Branch *branch = (Branch *)[_fetchedResultsController objectAtIndexPath:indexPath];
@@ -163,12 +168,56 @@
 }
 
 #pragma mark - NSFetchedResultsControllerDelegate
+- (void)controllerWillChangeContent:(NSFetchedResultsController *)controller
+{
+    [self.tableView beginUpdates];
+}
+
+- (void)controller:(NSFetchedResultsController *)controller
+  didChangeSection:(id <NSFetchedResultsSectionInfo>)sectionInfo
+           atIndex:(NSUInteger)sectionIndex
+     forChangeType:(NSFetchedResultsChangeType)type
+{
+    switch(type) {
+        case NSFetchedResultsChangeInsert:
+            [self.tableView insertSections:[NSIndexSet indexSetWithIndex:sectionIndex] withRowAnimation:UITableViewRowAnimationAutomatic];
+            break;
+        case NSFetchedResultsChangeDelete:
+            [self.tableView deleteSections:[NSIndexSet indexSetWithIndex:sectionIndex] withRowAnimation:UITableViewRowAnimationAutomatic];
+            break;
+    }
+}
+
+- (void)controller:(NSFetchedResultsController *)controller
+   didChangeObject:(id)object
+       atIndexPath:(NSIndexPath *)indexPath
+     forChangeType:(NSFetchedResultsChangeType)type
+      newIndexPath:(NSIndexPath *)newIndexPath
+{
+    switch(type) {
+        case NSFetchedResultsChangeInsert:
+            [self.tableView insertRowsAtIndexPaths:[NSArray arrayWithObject:newIndexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
+            break;
+        case NSFetchedResultsChangeDelete:
+            [self.tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
+            break;
+        case NSFetchedResultsChangeUpdate:
+            
+            [self configureCell:[self.tableView cellForRowAtIndexPath:indexPath] forRowAtIndexPath:indexPath];
+            break;
+        case NSFetchedResultsChangeMove:
+            [self.tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
+            [self.tableView insertRowsAtIndexPaths:[NSArray arrayWithObject:newIndexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
+            break;
+    }
+}
 
 - (void)controllerDidChangeContent:(NSFetchedResultsController *)controller {
     
     DDLogInfo(@"reloading table");
     //[self newLastModifiedValue];
-    [self.tableView reloadData];
+    [self.tableView endUpdates];
+    //[self.tableView reloadData];
 }
 
 - (void)newLastModifiedValue
@@ -195,6 +244,9 @@
     
     DDLogInfo(@"before reloading table, max lastModified is :%@", [oldest lastModified]);
 }
+
+#pragma mark - Tableview Animation
+
 
 
 @end
